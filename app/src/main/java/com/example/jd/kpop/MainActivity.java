f6071d4f91b2d67d110c43f2f6956c2055f11ba6
ejrejrwo <ejrejrwo@naver.com>
2020-01-30 03:30:42 +0900

    화면이동

--- app/src/main/java/com/example/jd/kpop/MainActivity.java
+++ app/src/main/java/com/example/jd/kpop/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.jd.kpop;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(getApplicationContext(), "OnCreate 호출됨", Toast.LENGTH_SHORT).show();
+
+        Button button = (Button) findViewById(R.id.gameBtn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Main2Activity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
